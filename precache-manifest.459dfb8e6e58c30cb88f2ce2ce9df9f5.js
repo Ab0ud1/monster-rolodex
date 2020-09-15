7a09ef1e5d20f92edba1101d8279637ab564dab8
Itani Abdul Kader <itani.aboudi@gamil.com>
2020-09-15 22:16:15 +0300

    Updates

---
+++ precache-manifest.459dfb8e6e58c30cb88f2ce2ce9df9f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084ed50a4cf1bf947bef0faf92c41235",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5749d23a03661aba674f",
+    "url": "/monster-rolodex/static/css/main.1fc11061.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5749d23a03661aba674f",
+    "url": "/monster-rolodex/static/js/main.42d23eef.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
